b4d7890f57fedb918612083a43703111c51e56fe
lucasbiazonc <lucas.crosatti@escola.pr.gov.br>
2026-03-19 11:57:46 -0300

    tem que arruma

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-// run `node index.js` in the terminal
-
-console.log(`Hello Node.js v${process.versions.node}!`);
+let n1 = 56;
+let n2 = 98;
+const soma = n1 + n2;
+const sub = n1 - n2
+let mensagem = 'O resultado da soma é ${soma}, e o resultado da subtração é ${sub}';
+console.log (mensagem);
+ if 
+ let msg2 = soma >=100? 'Sua soma é maior que 100', 'Sua soma é menor que 100'
+console.log (msg2)
