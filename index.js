37475127066316c2107237152e6d7debad4779b1
lucasbiazonc <lucas.crosatti@escola.pr.gov.br>
2026-03-20 08:03:49 -0300

    ta praticamente terminado

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ let n1 = 56;
 let n2 = 98;
 const soma = n1 + n2;
 const sub = n1 - n2
-let mensagem = 'O resultado da soma é ${soma}, e o resultado da subtração é ${sub}';
+let mensagem = 'O resultado da soma é {soma}, e o resultado da subtração é {sub}'.replace('{soma}', soma) .replace('{sub}', sub);
+let msg2 = soma >=100? 'Sua soma é maior que 100': 'Sua soma é menor que 100';
+let msg = soma >=100? 'Sua subtração é maior que 100': 'Sua subtração é menor que 100';
 console.log (mensagem);
- if 
- let msg2 = soma >=100? 'Sua soma é maior que 100', 'Sua soma é menor que 100'
-console.log (msg2)
+console.log (msg2);
+console.log (msg);
